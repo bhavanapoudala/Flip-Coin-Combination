463ca7e8346679731953e60c931034e7b3a3bc04
bhavana <bhavanapoudala9@gmail.com>
2021-03-03 12:48:09 +0530

    Added: Storing in Dictionary and Found Percentages

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
-
+count=0
+H=0
+T=0
+declare -A dict
+while [ $count -lt 20 ]
+do
 coin=$((RANDOM%2))
 if [ $coin -eq 1 ]
 then
-	echo "HEADS"
+	H=$(($H+1))
+        dict[$count]="HEADS"
 else
-	echo "TAILS"
+	T=$(($T+1))
+        dict[$count]="TAILS"
 fi
+((count++))
 
+done
+echo ${dict[@]}
+echo "heads are: " $H
+echo "tails are: " $T
+percentageH=$((100*$H/20))
+percentageT=$((100*$T/20))
+echo "percentage of Heads is: " $percentageH
+echo "percentage of Tails is: " $percentageT
 
